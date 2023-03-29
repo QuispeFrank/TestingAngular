5a38da425738df1bd0a614457c8026958d1b6d3b
FranQ <frankchristopherquispe@gmail.com>
2023-03-29 14:46:49 -0500

    a little more about basic event binding

--- mi-primera-app/src/app/people/people.component.ts
+++ mi-primera-app/src/app/people/people.component.ts
@@ -9,8 +9,13 @@ import { Component } from "@angular/core";
 export class PeopleComponent {
     disabled = false;
     message = "haven't added a person yet";
+    title = 'no title';
 
     addPerson() {
         this.message = 'person added :D'
+    }
+
+    changeTitle(event: Event) {
+       this.title = (<HTMLInputElement>event.target).value;
     }
 }
