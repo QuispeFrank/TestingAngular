169a3fbacb728d89aa4df890a8e47e6abc72e7e3
FranQ <frankchristopherquispe@gmail.com>
2023-03-28 21:40:42 -0500

    looks like folks are getting stylish with css

--- mi-primera-app/src/app/people/people.component.ts
+++ mi-primera-app/src/app/people/people.component.ts
@@ -4,6 +4,7 @@ import { Component } from "@angular/core";
 @Component ({
     selector: 'app-people',
     templateUrl: './people.component.html',
+    styleUrls: ['./people.component.css']
 })
 export class PeopleComponent {
 
