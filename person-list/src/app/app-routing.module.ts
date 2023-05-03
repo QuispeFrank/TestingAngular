a2167d6663f261848efa3c68e3980dc697f5f8cf
FranQ <frankchristopherquispe@gmail.com>
2023-05-03 00:31:58 -0500

    routing module and routing-oulet

--- person-list/src/app/app-routing.module.ts
+++ person-list/src/app/app-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { PeopleComponent } from './people/people.component';
+import { FormComponent } from './people/form/form.component';
+
+const routes: Routes = [
+  {path: '', component: PeopleComponent},
+  {path: 'people', component: PeopleComponent},
+  {path: 'people/add', component: FormComponent},
+  {path: 'people/:id', component: FormComponent}
+]
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forRoot(
+      routes
+    )
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
