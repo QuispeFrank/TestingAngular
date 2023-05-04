f6378fa4357a6ba0e97e626b71e889b0edc8d3a2
FranQ <frankchristopherquispe@gmail.com>
2023-05-04 17:33:30 -0500

    child Route

--- person-list/src/app/app-routing.module.ts
+++ person-list/src/app/app-routing.module.ts
@@ -4,10 +4,13 @@ import { PeopleComponent } from './people/people.component';
 import { FormComponent } from './people/form/form.component';
 
 const routes: Routes = [
-  {path: '', component: PeopleComponent},
-  {path: 'people', component: PeopleComponent},
-  {path: 'people/add', component: FormComponent},
-  {path: 'people/:id', component: FormComponent}
+  { path: '', component: PeopleComponent },
+  {
+    path: 'people', component: PeopleComponent, children: [
+      { path: 'add', component: FormComponent },
+      { path: ':id', component: FormComponent }
+    ]
+  }
 ]
 
 
